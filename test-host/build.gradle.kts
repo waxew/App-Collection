cd1aee33ecd6112ec475e49c4d79a0c75cbb1135
waxew <4622.ar@gmail.com>
2026-08-29 01:26:49 +0330

    Configure Android plugins for single test host

---
+++ test-host/build.gradle.kts
@@ -0,0 +1,6 @@
+// Plugin versions for the standalone single-APK test host build.
+// They intentionally match the main App-Collection project.
+plugins {
+    id("com.android.application") version "9.3.0" apply false
+    id("com.android.library") version "9.3.0" apply false
+}
